ce491678bc6d93677ee32551267cce33fdaa8f52
Scott Kleinman <scott.kleinman@csun.edu>
2021-03-12 14:47:48 -0800

    Update validate_manifest.py for v2.0

--- scripts/validate_manifest.py
+++ scripts/validate_manifest.py
@@ -1,33 +1,60 @@
-## Imports
-import requests, json
+"""validate_manifest.py.
+
+Usage Example:
+
+schema = 'https://raw.githubusercontent.com/whatevery1says/manifest/master/schema/v2.0/Corpus/Data.json'
+manifest = 'https://raw.githubusercontent.com/whatevery1says/manifest/master/examples/Data_sample.json'
+validation = Validation(schema)
+validation.is_valid(manifest)
+"""
+
+# Python imports
+import json
+import requests
 from jsonschema import validate, FormatChecker
 
-## Configure from list of manifest_types below
-manifest_file = 'https://raw.githubusercontent.com/whatevery1says/manifest/master/schema/Publications/sample.json'
-manifest_type = 'Publications/Publications'
+# Validation class
+class Validation():
+    """Validate a manifest according to a schema."""
 
-# These variables only rarely need to be modifed
-manifest_types = ['Publications/Publications', 'Corpus/collection', 'Corpus/RawData', 'Corpus/ProcessedData', 'Corpus/Metadata', 'Corpus/Outputs', 'Corpus/Related', 'Corpus/Data', 'Corpus/PathNode', 'Processes/Processes', 'Processes/Step', 'Scripts/Scripts', 'Project/Project']
-unavailable = ['Processes/Processes', 'Processes/Step', 'Scripts/Scripts', 'Project/Project']
+    def __init__(self, schema):
+        """Instantiate the Validation object.
 
-## Do not edit below this line
+        Takes as input a filepath or url to a json schema doc,
+        or a dict in json schema format.
+        """
+        self.schema_path = schema
+        self.schema = self.load_schema()
 
-# Read the schema file
-url = 'https://raw.githubusercontent.com/whatevery1says/manifest/master/schema/'
-schema_file = url + manifest_type + '.json'
-schema = requests.get(schema_file).text
-# print(schema)
-schema = json.loads(schema)
+    def load_json(self, path, type='schema'):
+        """Load json from url or file."""
+        if isinstance(path, dict):
+            return path
+        elif path.startswith('http'):
+            try:
+                return requests.get(path).json()
+            except:
+                print(f'Cannot find {type} at the designated location.')
+                return {}
+        else:
+            try:
+                with open(path) as f:
+                    doc = json.loads(f.read())
+                return doc
+            except IOError:
+                print(f'Cannot find {type} at the designated location.')
+                return {}
 
-# Read the manifest file
-with open(manifest_file) as f:
-    manifest = f.read()
+    def load_manifest(self, path):
+        """Load manifest from url or file."""
+        return self.load_json(path, type='manifest')
 
-def valid(manifest, schema):
-    # Wrapper to return feedback if the manifest is valid
-    validate(manifest, schema, format_checker=FormatChecker())
-    return True
+    def load_schema(self):
+        """Load schema from url or file."""
+        return self.load_json(self.schema_path, type='schema')
 
-# Validate the manifest
-is_valid = valid(manifest, schema)
-print(is_valid)
+    def is_valid(self, path):
+        """Validate the manifest and return a Boolean or raise an error."""
+        manifest = self.load_manifest(path)
+        validate(manifest, self.schema, format_checker=FormatChecker())
+        return True
